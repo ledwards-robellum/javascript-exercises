20122953ed3046db29ba7b8128fda3f0c229cc7a
Logan Edwards <ledwards@robellum.net>
2026-07-08 14:21:09 -0500

    Update 07_reverseString to pass tests
    Also update spec to run all tests

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let seperatedString = str.split("");
+    let sortReversed = seperatedString.sort((a, b) => a - b).reverse();
+    let joinedString = sortReversed.join("");
+    return joinedString;
 };
+
+reverseString("hello there")
+reverseString("123! abc! Hello, Odinite.")
+reverseString("")
 
 // Do not edit below this line
 module.exports = reverseString;
