e3b76699cdca507a18e81fd922f07765da48c077
Logan Edwards <ledwards@robellum.net>
2026-07-08 14:00:34 -0500

    Update 06_repeatString to pass tests
    Also modified spec to run all tests

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str, amt) {
+    let output = '';
+    if (amt >= 0) {
+        for (let i = 0; i < amt; i++) {
+            output += str;
+        }
+        return output;
+    } else {
+        return output = 'ERROR';
+    }
 };
 
+repeatString('hey', 3)
+repeatString('bye', 0)
 // Do not edit below this line
 module.exports = repeatString;
