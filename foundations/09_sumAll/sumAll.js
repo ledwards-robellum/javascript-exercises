35029f8b14a76008abe374d3f3a461190cb1a7ab
Logan Edwards <ledwards@robellum.net>
2026-07-13 16:34:30 -0500

    Update sumAll.js to pass tests

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+}
 
+const sumAll = function(...arr) {
+    let sortedArr = arr.sort(compareNumeric);
+    let newArr = [];
+    let sum = 0;
+
+    for (let i = sortedArr[0]; i <= sortedArr[1]; i++) {
+        newArr = newArr.concat([i]);
+    }
+    return sum = newArr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
+sumAll(1,4);
 // Do not edit below this line
 module.exports = sumAll;
