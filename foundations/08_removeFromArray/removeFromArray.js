de350bc8b23d98c61a81006dcad5a0937a91a620
Logan Edwards <ledwards@robellum.net>
2026-07-13 15:07:40 -0500

    Update removeFromArray.js to pass tests
    Also update spec to run all tests

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,18 +1,15 @@
-const removeFromArray = function(arr, ...remove) {
-    for (const item of remove) {
-        console.log(item);
-    }
+const removeFromArray = function(arr, ...args) {
+    return arr.filter(val => !args.includes(val))
 };
 
-//removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
+removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
 removeFromArray([1, 2, 3, 4], 3, 2);
-/* removeFromArray([1, 2, 2, 3], 2);
+removeFromArray([1, 2, 2, 3], 2);
 removeFromArray([1, 2, 3, 4], 7, "tacos");
 removeFromArray([1, 2, 3, 4], 7, 2);
 removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
 removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
 removeFromArray([1, 2, 3], "1", 3);
-*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
