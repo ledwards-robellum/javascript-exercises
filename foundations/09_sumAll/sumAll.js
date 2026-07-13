6ae3ec9565ed5a99889ab807e8e1c78ce72673f2
Logan Edwards <ledwards@robellum.net>
2026-07-13 18:26:21 -0500

    Add comments

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,7 @@
 const sumAll = function(min, max) {
-  if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
-  if (min < 0 || max < 0) return "ERROR";
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR'; // Check if NOT integer
+  if (min < 0 || max < 0) return "ERROR"; // Check if negative
+  // Reverse if min is larger than max
   if (min > max) {
     const temp = min;
     min = max;
@@ -8,6 +9,7 @@ const sumAll = function(min, max) {
   }
 
   let sum = 0;
+  // Iterate between min and max, adding to sum each time
   for (let i = min; i <= max; i++) {
     sum += i;
   }
