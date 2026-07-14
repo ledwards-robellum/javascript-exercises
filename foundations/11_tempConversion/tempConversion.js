4e742c56ee3e1f28621ac0509e300ac61b1543ec
Logan Edwards <ledwards@robellum.net>
2026-07-13 21:08:35 -0500

    Update functions to be more readable
    Now declarations and not expressions.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,10 +1,10 @@
-const convertToCelsius = function(temp) {
+function convertToCelsius(temp) {
   return Math.round(((temp - 32) / 1.8) * 10) / 10;
-};
+}
 
-const convertToFahrenheit = function(temp) {
+function convertToFahrenheit(temp) {
   return Math.round(((temp * 1.8) + 32) * 10) / 10;
-};
+}
 
 // Do not edit below this line
 module.exports = {
