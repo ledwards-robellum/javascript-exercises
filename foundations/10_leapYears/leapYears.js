638c79fb4d2f54eade7c3086fe1d6535eebb43c4
Logan Edwards <ledwards@robellum.net>
2026-07-13 19:09:39 -0500

    Update leapYears.js to pass tests
    Also update spec to run all tests

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+  // Leap year rule booleans
+  const isDivisibleByFour = inputYear % 4 === 0;
+  const isCentury = inputYear % 100 === 0;
+  const isDivisibleByFourHundred = inputYear % 400 === 0;
 
+  if (isDivisibleByFour &&
+      (!isCentury || 
+      isDivisibleByFourHundred)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
